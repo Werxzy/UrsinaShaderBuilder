1859b4d83a03facb25e67b19e6cba96376c7fd4a
Werxzy <Werxzyz@yahoo.com>
2022-02-05 04:30:40 -0600

    Added a variable for the name of the output.

--- node_builtin_output.py
+++ node_builtin_output.py
@@ -11,9 +11,11 @@ class BuiltInOutputNode(ShaderNode):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.ui_name = self.append_text('p3d_FragColor', size = 0.8)
-        # self.ui_name = self.append_text('color', size = 0.8)
+        self.variable_name = 'p3d_FragColor'
+        #self.variable_name = 'color'
 
+        self.ui_name = self.append_text(self.variable_name, size = 0.8)
+        
         self.ui_back = self.build_back()
 
         self.build_connector('vec4', ['vec4'], False, 0.157)
