68eacce253e400d491d9c2c337a3671b59783ddf
Werxzy <Werxzyz@yahoo.com>
2022-02-05 04:32:57 -0600

    Changed nodes to a class variable shader_nodes.

--- shader_builder_manager.py
+++ shader_builder_manager.py
@@ -13,19 +13,19 @@ class ShaderBuilderManager(Entity):
         super().__init__(parent = camera.ui)
         # temp model/color
 
-        nodes = []
+        self.shader_nodes = list()
 
         for key, value in kwargs.items():
             setattr(self, key, value)
 
         #test node
-        nodes.append(InstructionNode(parent = self, manager = self, instruction = 'Add', position = (-0.3,0)))
-        nodes.append(InstructionNode(parent = self, manager = self, instruction = 'Subtract', position = (0,0.3)))
-        nodes.append(InstructionNode(parent = self, manager = self, instruction = 'Clamp', position = (0.3,0)))
-        nodes.append(ConstantNode(parent = self, manager = self, data_type='vec4', position = (-0.6,-0.3)))
-        nodes.append(ConstantNode(parent = self, manager = self, data_type='vec3', position = (-0.3,-0.3)))
-        nodes.append(ConstantNode(parent = self, manager = self, data_type='vec2', position = (0.0,-0.3)))
-        nodes.append(BuiltInOutputNode(parent = self, manager = self, position = (0.4,-0.3)))
+        self.shader_nodes.append(InstructionNode(parent = self, manager = self, instruction = 'Add', position = (-0.3,0)))
+        self.shader_nodes.append(InstructionNode(parent = self, manager = self, instruction = 'Subtract', position = (0,0.3)))
+        self.shader_nodes.append(InstructionNode(parent = self, manager = self, instruction = 'Clamp', position = (0.3,0)))
+        self.shader_nodes.append(ConstantNode(parent = self, manager = self, data_type='vec4', position = (-0.6,-0.3)))
+        self.shader_nodes.append(ConstantNode(parent = self, manager = self, data_type='vec3', position = (-0.3,-0.3)))
+        self.shader_nodes.append(ConstantNode(parent = self, manager = self, data_type='vec2', position = (0.0,-0.3)))
+        self.shader_nodes.append(BuiltInOutputNode(parent = self, manager = self, position = (0.4,-0.3)))
 
 
     #quickly organize the nodes based on how the inputs/outputs are connected
