cdd4087d63b0413bb9ca3807627534e80af095b1
Werxzy <Werxzyz@yahoo.com>
2022-02-10 19:51:29 -0600

    Added mouse interaction to bar menu.

--- bar_menu.py
+++ bar_menu.py
@@ -23,15 +23,35 @@ class BarMenu(Entity):
             setattr(self, key, value)
 
         self.texts = []
+        self.colliders = []
         start_x = 0
-        height = 0
+        self._height = 0
         for key in self.options.keys():
             text = Text(key, parent = self, position = Vec3(start_x + self.text_spacing.x, -self.text_spacing.y, -0.1), scale = self.text_scale, color = self.color_text)
             if start_x > 0:
-                Entity(parent = self, position = Vec3(start_x, 0, -0.1), scale = Vec3(0.001, height, 1), origin_y = 0.5, model = 'quad', color = self.color_divider)
+                Entity(parent = self, position = Vec3(start_x, 0, -0.1), scale = Vec3(0.001, self._height, 1), origin_y = 0.5, model = 'quad', color = self.color_divider)
             else:
-                height = text.height + self.text_spacing.y * 2
-            start_x += text.width + self.text_spacing.x * 2
-            self.texts.append(text)
+                self._height = text.height + self.text_spacing.y * 2
+            
+            width = text.width + self.text_spacing.x * 2
 
-        self.back = Entity(parent = self, scale = Vec3(window.aspect_ratio, height, 1), origin = Vec3(-0.5,0.5,0), model = 'quad', color = self.color_back)
+            collider = Entity(parent = self, x = start_x, scale = Vec3(width, self._height, 1), origin = Vec3(-0.5,0.5,0), model = 'quad', collider = 'box', visible = False)
+
+            start_x += width
+            self.texts.append(text)
+            self.colliders.append(collider)
+
+        self.back = Entity(parent = self, scale = Vec3(window.aspect_ratio, self._height, 1), origin = Vec3(-0.5,0.5,0), model = 'quad', color = self.color_back)
+    
+    def input(self, key):
+        if key == 'left mouse down':
+            if mouse.hovered_entity in self.colliders:
+                i = self.colliders.index(mouse.hovered_entity)
+                op = self.options[self.texts[i].text]
+                if isinstance(op, dict):
+                    print(op) #TODO dropdown menu here
+                else:
+                    self.on_selected(op)
+
+    def update(self):
+        pass
