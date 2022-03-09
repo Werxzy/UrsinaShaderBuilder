352a592b242be93e6ac6296fe4159a4dc8c2c72d
Werxzy <Werxzyz@yahoo.com>
2022-03-08 20:02:31 -0600

    Added side buttons to each variable in the struct definition.

--- UrsinaShaderBuilder/Nodes/node_struct_definition.py
+++ UrsinaShaderBuilder/Nodes/node_struct_definition.py
@@ -1,6 +1,9 @@
+from ursina import *
+from instanced_box import InstancedBox
 from shader_instructions import DataTypes
 from shader_node import ShaderNode
 from ExtraData.color_atlas import *
+from ExtraData.extra_models import *
 
 '''
 Creates a definition of a struct
@@ -43,16 +46,63 @@ class StructDefinitionNode(ShaderNode):
         ui_name = self.append_value_input('Name', 'var')
         ui_type = self.append_drop_down('Type', dict((v,v) for v in DataTypes), self.on_selected, extra_info = ui_name)
         ui_array = self.append_drop_down('', StructDefinitionNode.array_options, on_select = self.on_array_change, extra_info = ui_name, set_to_key = True)
+        ui_buttons = self.append_side_buttons(ui_name)
         ui_div = self.append_divider()
-
+        
+        self.move_ui_section(ui_buttons, self.button, True)
         self.move_ui_section(ui_name, self.button, True)
         self.move_ui_section(ui_type, self.button, True)
         self.move_ui_section(ui_array, self.button, True)
         self.move_ui_section(ui_div, self.button, True)
+
         self.build_back()
 
-        self.section_groups.append([ui_name, ui_type, ui_array, [None], ui_div])
+        self.section_groups.append([ui_name, ui_type, ui_array, ui_buttons, [None], ui_div])
 
+
+    def append_side_buttons(self, section_starter):
+        button_size = 0.02
+        spacing = self.ui_spacing * 0.75
+        rounding = 0.01
+        extra = 0.01
+        quadScale = Vec2(button_size + spacing * 2, button_size * 3 + spacing * 4)
+        back = InstancedBox.main_group.new_entity(parent = self, 
+            box_scale = (quadScale.x * 0.5 - rounding + extra, quadScale.y * 0.5 - rounding, rounding * 2, rounding * 2), 
+            position = (-self.ui_build_width * 0.5 - button_size * 0.5 - spacing + extra, -quadScale.y * 0.5 + self.ui_build_pos - spacing, 0.2), 
+            color = c_node_dark)
+
+        x_start = -button_size * 0.5 - extra
+        y_start = button_size + spacing
+
+        self.create_button(back, button_size, (x_start, y_start, -0.01), up_arrow_vert, (section_starter, 0), c_node_light)
+        y_start -= spacing + button_size
+        self.create_button(back, button_size, (x_start, y_start, -0.01), down_arrow_vert, (section_starter, 1), c_node_light)
+        y_start -= spacing + button_size
+        self.create_button(back, button_size, (x_start, y_start, -0.01), x_vert, (section_starter, 2), c_red)
+
+        return self.append_ui_section((back, 0))
+        
+    def create_button(self, parent, size, position, model, data, color):
+
+        def input(key):
+            if key == 'left mouse down' and button.hovered:
+                self.button_press(button.data)
+
+        button = Entity(parent = parent,
+            model = Mesh(vertices=model, mode='ngon', static=False), 
+            scale = size * 0.25, 
+            collider = 'box',
+            position = position,
+            origin = (-2,0,0),
+            color = color)
+
+        button.data = data
+        button.input = input
+
+        return button
+
+    def button_press(self, data):
+        print(data)
 
     def on_selected(self, info, key):
         pass
